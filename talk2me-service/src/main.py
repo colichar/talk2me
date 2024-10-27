3d2193d175bbb3d1741ab55cd2f68714c1e7fdb5
Haris Colic <colicharis1@gmail.com>
2024-10-27 18:52:55 +0100

    feat: adds summarization  of text as service

--- talk2me-service/src/main.py
+++ talk2me-service/src/main.py
@@ -1,29 +1,56 @@
 from fastapi import FastAPI, File, UploadFile
-from transformers import WhisperProcessor, WhisperForConditionalGeneration
+from transformers import WhisperProcessor, WhisperForConditionalGeneration, pipeline
 import io
 import soundfile as sf
 import torch
 
 app = FastAPI()
 
-processor = WhisperProcessor.from_pretrained("openai/whisper-tiny")
-model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny")
-model.config.forced_decoder_ids = None
+# Initialize transcription model
+transcription_processor = WhisperProcessor.from_pretrained("openai/whisper-tiny")
+transcription_model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny")
+transcription_model.config.forced_decoder_ids = None
 
-@app.post("/process")
+# Initialize summarization model
+summarizer_model = pipeline("summarization", model="facebook/bart-large-cnn")
+
+async def transcribe_audio_file(file):
+    content = await file.read()
+
+    audio_array, sampling_rate = sf.read(io.BytesIO(content))
+
+    input_features = transcription_processor(audio_array, sampling_rate=sampling_rate, return_tensors="pt").input_features 
+
+    predicted_ids = transcription_model.generate(input_features)
+
+    transcription = transcription_processor.batch_decode(predicted_ids, skip_special_tokens=True)
+
+    return transcription[0]
+
+async def summarize_audio_file(file):
+    transcription = await transcribe_audio_file(file=file)
+
+    summary = summarizer_model(transcription, max_length=130, min_length=30, do_sample=False)
+
+    return summary[0]['summary_text']
+
+
+@app.post("/transcribe")
 async def process_audio(file: UploadFile = File(...)):
     try:
-        content = await file.read()
+        transcription = await transcribe_audio_file(file=file)
 
-        audio_array, sampling_rate = sf.read(io.BytesIO(content))
+        return {"transcription": transcription}
 
-        input_features = processor(audio_array, sampling_rate=sampling_rate, return_tensors="pt").input_features 
+    except Exception as e:
+        return {"error": str(e)}
 
-        predicted_ids = model.generate(input_features)
+@app.post("/summarize")
+async def process_audio(file: UploadFile = File(...)):
+    try:
+        summary = await summarize_audio_file(file=file)
 
-        transcription = processor.batch_decode(predicted_ids, skip_special_tokens=True)
-
-        return {"transcription": transcription[0]}
+        return {"summary": summary}
 
     except Exception as e:
         return {"error": str(e)}
