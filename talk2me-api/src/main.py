da23d88c8338c4cddc968c11682299bfd382732e
Haris Colic <colicharis1@gmail.com>
2024-11-04 20:34:53 +0100

    refactor: separate routes from main for api

--- talk2me-api/src/main.py
+++ talk2me-api/src/main.py
@@ -1,11 +1,10 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException, Form
+from fastapi import FastAPI
+from routes import api_router
 from fastapi.middleware.cors import CORSMiddleware
-from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
-import httpx
 
 app = FastAPI()
 
-SERVICE_URL = "http://localhost:8010/"
+app.include_router(api_router)
 
 # Configure CORS
 app.add_middleware(
@@ -15,50 +14,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-@app.post("/upload")
-async def upload_file(audio_file: UploadFile = File(...), operation: str = Form(...)):
-    try:
-        content = await audio_file.read()
-
-        new_url = SERVICE_URL + operation
-
-        if operation == 'transcribe':
-            async with httpx.AsyncClient() as client:
-                files = {'file': (audio_file.filename, content, audio_file.content_type)}
-                response = await client.post(new_url, files=files)
-        elif operation == 'summarize':
-            async with httpx.AsyncClient() as client:
-                files = {'file': (audio_file.filename, content, audio_file.content_type)}
-                response = await client.post(new_url, files=files)
-
-        else:
-            raise HTTPException(status_code=400, detail='Invalid operation')
-
-        if response.status_code == 200:
-            processed_audio = response.json()
-            return {
-                "message": "File processed successfully",
-                "result": processed_audio
-            }
-        else:
-            raise HTTPException(
-                status_code=response.status_code,
-                detail="Processing service failed to process audio file."
-            )
-
-    except httpx.RequestError as e:
-        raise HTTPException(
-            status_code=HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Error communicating with processing service: {stre(e)}"
-        )
-     
-    except Exception as e:
-        print(e)
-        raise HTTPException(
-            status_code=HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"An unexpected error occured: {str(e)}"
-        )
 
 if __name__ == "__main__":
     import uvicorn
---
+++ talk2me-api/src/routes.py
@@ -0,0 +1,51 @@
+from fastapi import APIRouter, UploadFile, File, HTTPException, Form
+from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
+import httpx
+
+api_router = APIRouter()
+
+SERVICE_URL = "http://localhost:8010/"
+
+@api_router.post("/upload")
+async def upload_file(audio_file: UploadFile = File(...), operation: str = Form(...)):
+    try:
+        content = await audio_file.read()
+
+        new_url = SERVICE_URL + operation
+
+        if operation == 'transcribe':
+            async with httpx.AsyncClient() as client:
+                files = {'file': (audio_file.filename, content, audio_file.content_type)}
+                response = await client.post(new_url, files=files)
+        elif operation == 'summarize':
+            async with httpx.AsyncClient() as client:
+                files = {'file': (audio_file.filename, content, audio_file.content_type)}
+                response = await client.post(new_url, files=files)
+
+        else:
+            raise HTTPException(status_code=400, detail='Invalid operation')
+
+        if response.status_code == 200:
+            processed_audio = response.json()
+            return {
+                "message": "File processed successfully",
+                "result": processed_audio
+            }
+        else:
+            raise HTTPException(
+                status_code=response.status_code,
+                detail="Processing service failed to process audio file."
+            )
+
+    except httpx.RequestError as e:
+        raise HTTPException(
+            status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Error communicating with processing service: {str(e)}"
+        )
+     
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An unexpected error occured: {str(e)}"
+        )
